8e1e7b0b939162d7cb6ea65e98b71a17936d7e5b
RazazanOfficial <RazazanOfficial@gmail.com>
2025-02-13 17:23:01 +0330

    02.Intermediate ---> SE12 SE13 SE14 SE15 --> Done!

---
+++ 02.Intermediate/12. Map Helper/app.js
@@ -0,0 +1,4 @@
+const randomNum = [2, 3, 1, 4];
+const multNum = randomNum.map((num) => num * 10);
+
+console.log(multNum);
---
+++ 02.Intermediate/13. Filter Helper/app.js
@@ -0,0 +1,13 @@
+const ages = [32, 33, 16, 40];
+
+const adults = ages.filter((a) => a > 18);
+
+console.log(adults);
+
+///////////////////////////////////////////////////////
+
+const words = ["apply", "oranges", "Hello", "print"];
+
+const longWords = words.filter(w=>w.length >6)
+
+console.log(longWords);
